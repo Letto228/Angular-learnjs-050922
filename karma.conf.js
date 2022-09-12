c8d214490baf5e4fb3899c26c878d84ea133c02a
Oxenz <freelancepurework@gmail.com>
2022-09-12 14:18:35 +0300

    ci(**): applying basic formatting

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,14 @@ module.exports = function (config) {
 		coverageReporter: {
 			dir: require('path').join(__dirname, './coverage/angular-learnjs-050922'),
 			subdir: '.',
-			reporters: [{ type: 'html' }, { type: 'text-summary' }],
+			reporters: [
+				{
+					type: 'html',
+				},
+				{
+					type: 'text-summary',
+				},
+			],
 		},
 		reporters: ['progress', 'kjhtml'],
 		port: 9876,
