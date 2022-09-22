4d70c4e214cce6cbbfc178f9f689371304e3565b
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-09-22 17:07:12 +0300

    feat: lecture 4 decision

--- src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -1,9 +1,55 @@
-import { Directive, EventEmitter, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { LoadDirection } from './load-direction.const';
+import { isScrollReachedBottom, isScrollReachedTop } from './utils';
 
 @Directive({
 	selector: '[appScrollWithLoading]',
 })
 export class ScrollWithLoadingDirective {
+	@Input() isLoadingData = false;
+
 	@Output() loadData = new EventEmitter<LoadDirection>();
+
+	private readonly scrollElement: HTMLElement = this.elementRef.nativeElement;
+
+	private prevScrollTop = -1;
+
+	@HostListener('scroll')
+	onScroll() {
+		const prevScrollTop = this.prevScrollTop;
+
+		this.prevScrollTop = this.scrollTop;
+
+		if (this.isLoadingData) {
+			return;
+		}
+
+		const shouldLoadMessagesDown = isScrollReachedBottom(this.scrollTop, this.lowerScrollPosition, prevScrollTop);
+
+		if (shouldLoadMessagesDown) {
+			this.loadData.emit(LoadDirection.After);
+
+			return;
+		}
+
+		const shouldLoadMessagesTop = isScrollReachedTop(this.scrollTop, prevScrollTop);
+
+		if (shouldLoadMessagesTop) {
+			this.loadData.emit(LoadDirection.Before);
+
+			return;
+		}
+	}
+
+	private get scrollTop(): number {
+		return this.scrollElement.scrollTop;
+	}
+
+	private get lowerScrollPosition(): number {
+		const { clientHeight, scrollHeight } = this.scrollElement;
+
+		return scrollHeight - clientHeight;
+	}
+
+	constructor(private readonly elementRef: ElementRef) {}
 }
